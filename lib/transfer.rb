a7f85a785de07dbd8a2635b511b6cc75f9f9256c
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 22:39:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,7 @@ class Transfer
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount 
     @status = "complete"
+    binding.pry
     @sender.balance
     @receiver.balance 
   end 
