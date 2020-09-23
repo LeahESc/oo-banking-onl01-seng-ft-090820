54c9edc2f0fccdf0d85acfdf81e025285739f5ad
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 23:07:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
   
   def execute_transaction 
     # binding.pry
-    if  @sender.balance < 0 
+    if  @sender.balance < 0 || @sender.status == "closed"
       @status = "rejected"
       "Transaction rejected. Please check your account balance." 
       # binding.pry
