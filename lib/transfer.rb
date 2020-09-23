c999153b7506a2129b299d23ded528f52f9b7e53
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 21:51:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,11 @@
 class Transfer
  
-  attr_accessor :sender, :receiver 
+  attr_accessor :sender, :receiver, :status
   
-  def initialize(sender)
+  def initialize(sender, receiver, status = "pending")
     @sender = sender
     @receiver = receiver
-    @status = pending 
+    @status = "pending" 
   end
 
 end
