c4fef9962fffd68df47fba31aa466c9fe2a1c2b8
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 22:50:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,8 +27,8 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount 
       @status = "complete"
-      define_singleton_method(:execute_transaction) {}
     end
+    define_singleton_method(:execute_transaction) {}
   end 
   
 end
