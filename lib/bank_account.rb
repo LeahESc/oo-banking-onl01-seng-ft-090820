d9cf43ec6aa023ba27fed84366ce2bd46c43dac0
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 22:11:00 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -21,7 +21,7 @@ class BankAccount
   
   def valid? 
     # binding.pry
-     @status = "open" && @balance > 0 ? true : false
+     @status = "open" and @balance > 0 ? true : false
     #   true 
     # else 
     #   @status = false
